2ea533502a03e2c880e41d6036180aab1bff37f5
Vincius Silva Barros <viniciussilvabarros05@hotmail.com>
2021-08-21 20:02:42 -0300

    Finish

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 
 let view = document.querySelector(".model")
-let view1 = document.querySelector(".model1")
-let view2 = document.querySelector(".model2")
-let th = document.querySelectorAll("th")
-
 let table = document.getElementById("table")
-let table1 = document.getElementById("table1")
-let table2 = document.getElementById("table2")
+let th = document.querySelectorAll("th")
 
 let input_name = document.getElementById("name")
 let product = document.getElementById("product")
 let alerta = document.querySelector(".alerta")
 
-let total_comission = document.querySelector(".total_comissão")
-let calc_item = document.getElementsByClassName("soma_item")
 
 let list_product = [
     {
@@ -141,39 +134,82 @@ let atendentes = [
     {
         id: 0,
         name: "Vinicius Silva",
-        lista: []
+        lista: [],
+        table: ` <table id = "table">
+        <thead>
+            <th>nome</th>
+            <th>preço</th>
+            <th>comissão</th>
+        </thead>
+       
+    </table>
+   
+    `
     },
     {
         id: 1,
         name: "Mateus Gomes",
-        lista: []
+        lista: [],
+        table: `<table id = "table">
+        <th>nome</th>
+        <th>preço</th>
+        <th>comissão</th>
+           
+       
+    </table>
+   
+    
+    `
     },
     {
         id: 2,
         name: "Rosângela Alves",
-        lista: []
+        lista: [],
+        table: `
+        <table id = "table">
+        <th>nome</th>
+        <th>preço</th>
+        <th>comissão</th>
+           
+        </table>
+       
+    
+       `
+    },
+
+    {
+        id: 3,
+        name: "Maicon Santos",
+        lista: [],
+        table: `
+        <table id = "table">
+        <th>nome</th>
+        <th>preço</th>
+        <th>comissão</th>
+           
+        </table>
+       
+    
+       `
     }]
 
 function check_clerk() {
     atendentes.forEach(i => {
-
-
-
         if (i.name == input_name.value) {
             push_item(i)
 
             product.value = ""
         }
     })
-
-    soma()
-
     const name = atendentes.filter((i) => { return i.name == input_name.value })
 
-    if (name =="") {
+    if (name == "") {
         return alerta.classList.add("view_actived")
     }
 }
+
+
+
 
 function push_item(a) {
     let vendas = {}
@@ -187,72 +223,144 @@ function push_item(a) {
             }
 
             return a.lista.push(vendas)
-
         }
-
-
     })
-
     if (product.value > 20 || !product.value || product.value < 1) {
         return alerta.classList.add("view_actived")
     }
-
-    listar(a.id, vendas.valor, vendas.nome)
-
-
 }
 
 
-function view_model(i) {
-    let array_view = [view, view1, view2]
-    array_view[i].classList.remove("view_actived")
+function view_model() {
+    view.classList.toggle("view_actived")
 }
 
-function back(i) {
-    let array_view = [view, view1, view2]
-    array_view[i].classList.toggle("view_actived")
-}
-
-function listar(i, valor, nome) {
-    let array_table = [table, table1, table2]
-    array_table[i].innerHTML += `<tbody">
-        <th>${nome}</th>
-        <th>R$${valor}</th>
-        <th>${soma()}</th>
-        </tbody>`
-
-
-
-}
-
-function soma() {
-    let soma = 0
-    let total = 0
-    let porcentagem
-    let results = []
+function back() {
 
 
     atendentes.forEach(i => {
-        i.lista.forEach(j => {
-            if (j.valor <= 16.10) {
-                porcentagem = 0.1
-            } else {
-                porcentagem = 0.15
+
+        if (event.target.innerHTML == i.name) {
+            table.innerHTML = i.table
+
+
+            for (index of i.lista) {
+
+
+                let tbody = `
+                
+                    <tbody>
+                        <th>${index.nome}</th>
+                        <th>${index.valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</th>
+                         <th>${soma(index.valor)}</th>
+                    </tbody>
+                `
+
+                table.innerHTML += tbody
+
             }
+            table.innerHTML += `<table>
+                <th>Total de Vendas</th>
+                <th id = "total_vendas">${total_vendas()}</th>
+                <th id="total_comissão">${total_comission()}</th>
+             </table>`
 
-            soma = j.valor * porcentagem
-            total = total + soma
-        })
-
-
+        }
 
     })
+    view.classList.toggle("view_actived")
+}
+
+function soma(a) {
+    let soma = 0
+    let somaTotal =0
+    let porcentagem
+
+    if (a <= 16.10) {
+        porcentagem = 0.1
+    } else {
+        porcentagem = 0.15
+    }
+    
+    soma = a * porcentagem
     const formatado = soma.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
-    const formatTotal = total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
-
-    total_comission.innerHTML = formatTotal
-
     return formatado
 
 }
 
+
+
+
+
+function total(a) {
+    let results = []
+    let result = []
+
+   
+
+    atendentes.forEach(i => {
+        if (event.target.innerHTML == i.name) {
+            i.lista.forEach(j => {
+
+                results.push(j.valor)
+                
+
+                let sum = results.reduce((total, value, index, array) => {
+                    return total + value
+                })
+
+                const formatTotal = sum.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+                a.push(formatTotal)
+                
+            })
+        }
+    })
+  
+}
+function calc_total_comission(a) {
+    let results = []
+    let result = []
+    let porcentagem
+
+    
+
+    atendentes.forEach(i => {
+        if (event.target.innerHTML == i.name) {
+            i.lista.forEach(j => {
+               
+                if (a <= 16.10) {
+                    porcentagem = 0.1
+                } else {
+                    porcentagem = 0.15
+                }
+                
+
+                results.push(j.valor*porcentagem)
+                
+
+                let sum = results.reduce((total, value, index, array) => {
+                    return total + value
+                })
+
+                const formatTotal = sum.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+                a.push(formatTotal)
+                
+            })
+        }
+    })
+  
+}
+
+
+function total_vendas(){
+    let array= []
+    total(array)
+   
+    return array[array.length-1]
+}
+function total_comission(){
+    let array= []
+    calc_total_comission(array)
+  
+    return array[array.length-1]
+}
